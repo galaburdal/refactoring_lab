b329b2ee67e3c4012e557ce799fe4f662f80e138
galaburdal <115738018+galaburdal@users.noreply.github.com>
2025-05-20 15:35:15 +0300

    Delete run directory

--- run/commands/order.py
+++
@@ -1,9 +0,0 @@
-class Order:
-    """Модель замовлення"""
-
-    def __init__(self, description: str, cost: float):
-        self.description = description
-        self.cost = cost
-
-    def __str__(self):
-        return f"Order: {self.description} | Total: ${self.cost:.2f}"
